8753117146cf45652a29e8592b6469694f8c3a14
Sarmad Rehman <sarmadr02@gmail.com>
2024-03-28 06:00:53 +0500

    completed

--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -75,7 +75,7 @@ function Details() {
                     d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"
                   />
                 </svg>
-                <span>Weighs 16 kg</span>
+                <span>Weighs&nbsp;20&nbsp;kg</span>
               </li>
             </ul>
             <div className="chair-price">
