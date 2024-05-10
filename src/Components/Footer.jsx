18a9c6e026ca8c7643119650f64084ffaa50df75
Sarmad Rehman <sarmadr02@gmail.com>
2024-05-10 16:51:57 +0500

    footer copyright removed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,9 @@
 function Footer() {
   return (
     <footer>
-      Copyright &copy; 2027 by Jonas Schmedtmann. Part of "Build Responsive
+      {/* Copyright &copy; 2027 by Jonas Schmedtmann. Part of "Build Responsive
       Real-World Websites with HTML and CSS" online course. Use for learning
-      purposes only.
+      purposes only. */}
     </footer>
   );
 }
